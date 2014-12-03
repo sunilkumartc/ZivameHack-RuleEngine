13dd63906c1b88d419fb76e39328655df1f594ba
sunilkumartc <sunilkumar.tc@hotmail.com>
2014-12-03 11:33:55 +0530

    Create adminInfo.py

---
+++ adminInfo.py
@@ -0,0 +1,14 @@
+#This file should be kept updated by admin
+
+
+category_range = 50
+
+priorityList = ["r4","r5","r3","r2","r7","r6","r9","r8","r1"]
+
+#attributes of list : discount%, expiry timestamp
+
+paymentOffers = {"COD" : ["-2%","16/11/14 23:59"], "CITI" : ["10%","14/11/14 23:59"], "SBI" : ["10%","19/11/14 23:59"] , "CANARA" : ["5%","17/11/14 23:59"], "HDFC" : ["10%","20/11/14 23:59"] , "ICICI" : ["0%","7/11/14 23:59"] }
+
+rechargeCodes = {"ABCD" : ["5%","18/11/14 23:59"], "XYZ123" : ["3%","19/11/14 23:59"] , "HAZLE" : ["4%","17/11/14 23:59"]}
+
+userType = {"FirstTime" : ["4%","15/11/14 23:59"] , "Defaulter" : ["-3%","20/12/14 23:59"] , "Loyal" : ["8%","18/11/15 23:59"]}
